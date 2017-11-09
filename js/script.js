b4dc3b887734cf9990ed383c60a3a8d96796f8a0
laurenweining <30637617+laurenweining@users.noreply.github.com>
2017-11-08 22:04:39 -0500

    Adding all Healing Hamper files.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+if(!localStorage["alertdisplayed"]) {
+    alert("There is still time to donate to Healing Hamper! See link at the top of the site to donate.")
+    localStorage["alertdisplayed"] = true
+}
